001cfe292ceca664e6da9580782ec8f623df3b67
Sandra Petereau <sandra.petereau@outlook.fr>
2024-04-19 15:59:31 +0200

    fix(dashboard): sort new bills

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,34 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { fireEvent, screen } from '@testing-library/dom'
+import NewBillUI from '../views/NewBillUI.js'
+import NewBill from '../containers/NewBill.js'
 
+describe('Given I am connected as an employee', () => {
+	describe('When I am on NewBill Page', () => {
+		// test('Then ...', () => {
+		// 	const html = NewBillUI()
+		// 	document.body.innerHTML = html
+		// 	//to-do write assertion
+		// })
+		describe('When I import a file', () => {
+			test('It should be an image', () => {
+				const html = NewBillUI()
+				document.body.innerHTML = html
+				const file = new File(['dummy content'], 'dummy.png', {
+					type: 'image/png',
+				})
+				const fileInput = screen.getByTestId('file')
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
+				fireEvent.change(fileInput, {
+					target: {
+						files: [file],
+					},
+				})
+
+				expect(fileInput.files[0].type).toBe('image/png')
+			})
+		})
+	})
 })
